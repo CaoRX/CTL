a7e4b1cae3d36fb429762b1e160d723bc43cf56a
Nan <umi@pku.edu.cn>
2021-06-17 18:36:37 +0900

    change to setuptools style

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name='CTL',
-    version='0.1.0',
-    author='R. Cao',
-    author_email='akks.crx@gmail.com',
-    packages=['CTL', 'CTL.tests'],
-    scripts=['bin/example.py','bin/hotrg-gilt-example.py'],
-    url='http://pypi.python.org/pypi/CTL/',
-    license='LICENSE.txt',
-    description='Tensor Network Library.',
-    long_description=open('README.md').read(),
-    install_requires=[
-        "numpy >= 1.20.0",
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="CTL",
+    version="0.0.1",
+    author="R. Cao",
+    author_email="akks.crx@gmail.com",
+    description="Tensor Network Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
 )
