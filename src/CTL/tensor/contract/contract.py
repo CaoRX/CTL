[bug]fix a broadcast bug for tensor contraction, with einsum

src/CTL/tensor/contract/contract.py
@@ -20,13 +20,13 @@ def shareBonds(ta, tb):
 # for each output element: we can decide it in O(1) time
 # so the complexity is just the shape of output tensor
 
-def contractTensors(ta, tb, bonds = None, outProductWarning = True):
+def contractTwoTensors(ta, tb, bonds = None, outProductWarning = True):
 	# contract between bonds(if not, then find the shared legs)
 	# this requires that: tensor contraction happens in-place
 	# after we prepare some tensor, we must create tensors to make links
 
 	if (not ta.diagonalFlag) and (tb.diagonalFlag):
-		return contractTensors(tb, ta, bonds = bonds, outProductWarning = outProductWarning)
+		return contractTwoTensors(tb, ta, bonds = bonds, outProductWarning = outProductWarning)
 	if (bonds is None):
 		bonds = shareBonds(ta, tb)
 	
@@ -71,19 +71,25 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 		# 1. calculate the core with broadcast
 		# 2. calculate the real tensor with np.outer
 		# how to broadcast?
-		tb.moveLegsToFront(contractBLegs)
+
+		# we need to broadcast from the end(instead of the first dimension)
+		# so we need to make the contract legs to the end
+		# then we need to take diagonal from these dimensions
+
+		tb.moveLegsToFront(tbRemainLegs)
 		dim = len(contractBLegs)
 		l = contractBLegs[0].dim
-
-		# print('shape a = {}, shape b = {}'.format(ta.shape, tb.shape))
+		# print('contract A legs: {}'.format(len(contractALegs)))
+		# print('contract B legs: {}'.format(len(contractBLegs)))
 		remADim = len(taRemainLegs)
-		# print('remADim = {}'.format(remADim))
-		data = tb.a[np.diag_indices(l, dim)] * ta.a 
-		# print('data = {}'.format(data))
+
+		einsumStr = '...' + ('j' * dim) + '->...j'
+		data = np.einsum(einsumStr, tb.a) * ta.a
 		if (remADim == 0):
-			newData = np.sum(data, axis = 0)
+			newData = np.sum(data, axis = -1)
 		else:
 			newData = np.outer(np.ones(tuple([l] * (remADim - 1))), data)
+		# print('newData = {}'.format(newData))
 		# print('shape = {}, data = {}, legs = {}'.format(newShape, newData, newLegs))
 
 		return Tensor(shape = newShape, data = newData, legs = newLegs)
@@ -96,7 +102,7 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 
 	return Tensor(shape = newShape, data = newData, legs = newLegs)
 
-# def contractTensorsByLabel(ta, tb, labels): # not in-place
+# def contractTwoTensorsByLabel(ta, tb, labels): # not in-place
 # 	labelA, labelB = labels
 # 	if (isinstance(labelA, str)):
 # 		label_a = [labelA]
@@ -109,7 +115,7 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 # 	bCopy = tb.copy()
 
 # 	bond_names = makeLink(aCopy, bCopy, (label_a, label_b))
-# 	res = contractTensors(ta_cp, tb_cp, cr = bond_names)
+# 	res = contractTwoTensors(ta_cp, tb_cp, cr = bond_names)
 # 	return res.copy()
 
 # def self_trace_square(tensor_tuple):
@@ -128,9 +134,9 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 # 	ld.renameLabel('d', 'd2')
 # 	ld.renameLabel('l', 'l1')
 
-# 	l = contractTensors(lu, ld)
-# 	r = contractTensors(ru, rd)
-# 	t = contractTensors(l, r)
+# 	l = contractTwoTensors(lu, ld)
+# 	r = contractTwoTensors(ru, rd)
+# 	t = contractTwoTensors(l, r)
 # 	t.outerProduct(['u1', 'u2'], 'u')
 # 	t.outerProduct(['d2', 'd1'], 'd')
 # 	t.outerProduct(['l1', 'l2'], 'l')
@@ -175,9 +181,9 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 # 	ald.renameLabel('d', 'd2')
 # 	ald.renameLabel('l', 'l1')
 
-# 	l = contractTensors(alu, ald)
-# 	r = contractTensors(aru, ard)
-# 	t = contractTensors(l, r)
+# 	l = contractTwoTensors(alu, ald)
+# 	r = contractTwoTensors(aru, ard)
+# 	t = contractTwoTensors(l, r)
 # 	t.outerProduct(['u1', 'u2'], 'u')
 # 	t.outerProduct(['d1', 'd2'], 'd')
 # 	t.outerProduct(['l1', 'l2'], 'l')
@@ -201,7 +207,7 @@ def contractTensors(ta, tb, bonds = None, outProductWarning = True):
 # 	makeLink(alu, aru, ('l', 'r'))
 # 	makeLink(ald, ard, ('l', 'r'))
 
-# 	l = contractTensors(alu, ald)
-# 	r = contractTensors(aru, ard)
-# 	t = contractTensors(l, r)
+# 	l = contractTwoTensors(alu, ald)
+# 	r = contractTwoTensors(aru, ard)
+# 	t = contractTwoTensors(l, r)
 # 	return t.a

src/CTL/tests/test_diagonal.py
@@ -3,8 +3,8 @@ from CTL.tensor.diagonalTensor import DiagonalTensor
 
 from CTL.tensor.tensor import Tensor 
 from CTL.tensor.contract.link import makeLink
-from CTL.tensor.contract.optimalContract import contractTensorList, generateOptimalSequence
-from CTL.tensor.contract.contract import contractTensors
+from CTL.tensor.contract.optimalContract import contractTensorList, generateOptimalSequence, contractCost
+from CTL.tensor.contract.contract import contractTwoTensors
 from CTL.tensor.contract.optimalContract import makeTensorGraph, contractAndCostWithSequence
 import CTL.funcs.funcs as funcs
 
@@ -37,6 +37,7 @@ class TestDiagonalTensor(PackedTest):
         self.assertEqual(diagonalTensor.trace(), 3.0)
 
     def test_contraction(self):
+        print('Begin test diagonalTensor contraction.')
         a = DiagonalTensor(shape = (2, 2), labels = ['a', 'b'])
         b = Tensor(shape = (2, ), labels = ['x'])
         c = Tensor(shape = (2, ), labels = ['y'])
@@ -75,6 +76,7 @@ class TestDiagonalTensor(PackedTest):
         makeLink('b', 'y', a, c)
 
         res1, cost1 = contractAndCostWithSequence([a, b, c])
+        print('seq = {}'.format(generateOptimalSequence([a, b, c])))
 
         a = Tensor(data = aData, labels = ['a', 'b', 'c'])
         b = Tensor(data = bData, labels = ['x'])
@@ -89,6 +91,46 @@ class TestDiagonalTensor(PackedTest):
         # print(cost1, cost2)
 
         # print(res1.a, res2.a)
+
+        # test diagonal tensor contraction
+        a = DiagonalTensor(shape = (2, 2), labels = ['a1', 'a2'])
+        b = DiagonalTensor(shape = (2, 2, 2), labels = ['b1', 'b2', 'b3'])
+        makeLink('a1', 'b1', a, b)
+        res = contractTwoTensors(a, b)
+        self.assertTupleEqual(res.shape, (2, 2, 2))
+        self.assertEqual(res.dim, 3)
+        self.assertTrue(res.diagonalFlag)
+        self.assertTrue((res.a == np.ones(2)).all())
+
+        # test for diagonal * diagonal contraction cost(just O(length))
+        a = DiagonalTensor(shape = (2, 2), labels = ['a1', 'a2'])
+        b = DiagonalTensor(shape = 2, labels = ['b1', 'b2']) # deduce dim
+        makeLink('a1', 'b2', a, b)
+
+        cost, _ = contractCost(a, b)
+        self.assertEqual(cost, 2.0)
+
+        res, cost = contractAndCostWithSequence([a, b])
+        self.assertEqual(res.dim, 2)
+        self.assertEqual(res._length, 2)
+        self.assertTupleEqual(res.shape, (2, 2))
+        self.assertEqual(cost, 2.0)
+        self.assertTrue(res.diagonalFlag)
+
+        a = DiagonalTensor(shape = (2, 2), labels = ['a1', 'a2'])
+        b = Tensor(shape = (2, 3, 3), labels = ['b1', 'b2', 'b3']) # deduce dim
+        makeLink('a1', 'b1', a, b)
+
+        cost, _ = contractCost(a, b)
+        self.assertEqual(cost, 18.0)
+
+        res, cost = contractAndCostWithSequence([a, b])
+        print(res)
+        self.assertEqual(res.dim, 3)
+        self.assertTrue(funcs.compareLists(list(res.shape), [2, 3, 3]))
+        self.assertEqual(cost, 18.0)
+
+
 
     def test_deduce(self):
 